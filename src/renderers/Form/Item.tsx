8e7c11f472779c91d54906571f09a1aa32033392
ryg319 <ryg18335844165@126.com>
2022-05-12 17:45:10 +0800

    对齐4.0样式

--- src/renderers/Form/Item.tsx
+++ src/renderers/Form/Item.tsx
@@ -1184,7 +1184,8 @@ export const detectProps = [
   'minLength',
   'maxLength',
   'embed',
-  'displayMode'
+  'displayMode',
+  "showSteps"
 ];
 
 export function asFormItem(config: Omit<FormItemConfig, 'component'>) {
